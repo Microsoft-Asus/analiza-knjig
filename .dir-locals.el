2a4df32290d28125b8ec8e8baea7ee82c62f8d65
ggasper <gg.gasper@gmail.com>
2019-11-04 17:53:39 +0100

    Removed emacs file for local env variables

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; For more information see (info "(emacs) Directory Variables")
-
-((python-mode
-  (eval progn   (make-local-variable 'process-environment)   (setenv "WORKON_HOME" "~/Projects/FMF/analiza-podatkov/"))))
-    
-   
